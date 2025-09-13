790c2841e5f9d3b3c56a74debcfcc7b86e174d3b
Likhon <likh.deshi@gmail.com>
2025-09-13 18:10:25 +0600

    Use the setup method

--- tests/unit/test_api/test_entry.py
+++ tests/unit/test_api/test_entry.py
@@ -1,16 +1,22 @@
 from http import HTTPStatus
 from fastapi.testclient import TestClient
+import pytest
 
 
 class TestEntry:
     """Test entry point routes."""
 
-    def test_root(self, client: TestClient) -> None:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, client: TestClient) -> None:
+        """Initialize."""
+        self.client = client
+
+    def test_root(self) -> None:
         """Test root endpoint."""
-        response = client.get("/")
+        response = self.client.get("/")
         assert response.status_code == HTTPStatus.OK
 
-    def test_health_check(self, client: TestClient) -> None:
+    def test_health_check(self) -> None:
         """Test health check."""
-        response = client.get("health")
+        response = self.client.get("health")
         assert response.status_code == HTTPStatus.OK
